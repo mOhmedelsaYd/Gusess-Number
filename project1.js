aebc74968a08bec3f9f1bd1eb140ba652008fd16
Mohamed Elsayed Ahmed <mohamedelsaid3963@gmail.com>
2023-04-12 17:37:30 +0200

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,55 @@
+let secretnumber = Math.trunc(Math.random() * 20 + 1);
+console.log(`${secretnumber} , ${typeof secretnumber}`);
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+
+    const guess = Number(document.querySelector(".guess").value);
+    if (!guess) {
+        document.querySelector(".message").textContent = '⛔ No number!';
+    } else if (guess > secretnumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = 'Too high!';
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = '💥 You lost the game!';
+        }
+    }
+    else if (guess < secretnumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = 'Too low!';
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = '💥 You lost the game!';
+        }
+    }
+    else if (guess === secretnumber) {
+        document.querySelector(".message").textContent = '🎉 Correct Number!';
+        document.querySelector(".number").textContent = secretnumber;
+        document.body.style.backgroundColor = '#60b347';
+        document.querySelector(".number").style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
+    }
+})
+
+document.querySelector(".again").addEventListener("click", function () {
+score = 20;
+    secretnumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(secretnumber,typeof secretnumber)
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+document.querySelector(".message").textContent = 'Start guessing...';
+document.querySelector('.score').textContent = score;
+document.querySelector('.number').textContent = '?';
+document.querySelector('.guess').value = '';
+
+document.querySelector('body').style.backgroundColor = '#222';
+document.querySelector('.number').style.width = '15rem';
+})
